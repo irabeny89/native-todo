43f1c65efafd768c6d5537d02b5905b64bf1ea0c
Irabeny <irabeny89@gmail.com>
2024-12-19 00:41:42 +0100

    feat: add home screen resolve #1

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,32 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.plusSign}>+</Text>
+      </View>
+      <View>
+        <Text style={styles.infoText}>
+          Tap to start adding what you want to do.
+        </Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  plusSign: {
+    fontSize: 95,
+  },
+  infoText: {
+    width: 200,
+    textAlign: "center",
+    fontSize: 14,
+  },
+});
