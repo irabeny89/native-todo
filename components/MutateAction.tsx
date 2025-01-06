97d9e1c619f405e3344d4930f09b372b052c0438
Babatunde Ademilua <babatundeademilua07@gmail.com>
2025-01-06 14:47:33 +0000

    feat: add menu in edit screen

--- components/MutateAction.tsx
+++ components/MutateAction.tsx
@@ -1,23 +1,50 @@
-import { StyleSheet, View } from "react-native";
+import { Pressable, StyleSheet, View } from "react-native";
 import UpdateBtn from "./updateTodoBtn";
 import DeleteBtn from "./DeleteTodoBtn";
+import { Fragment, useState } from "react";
+import { Entypo } from "@expo/vector-icons";
 
 export default function MutateAction() {
+  const [showOptions, setShowOptions] = useState(false);
   return (
-    <View style={styles.container}>
-      <View>
-        <DeleteBtn />
-      </View>
-      <View>
-        <UpdateBtn />
-      </View>
-    </View>
+    <Fragment>
+      <Pressable
+        onPress={() => setShowOptions(!showOptions)}
+        style={styles.menuIcon}
+      >
+        <Entypo name="dots-three-horizontal" size={24} color="black" />
+      </Pressable>
+      {showOptions && (
+        <View style={styles.actionCta}>
+          <View>
+            <DeleteBtn />
+          </View>
+          <View>
+            <UpdateBtn />
+          </View>
+        </View>
+      )}
+    </Fragment>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    gap: 10,
+  menuIcon: {
+    marginBottom: 10, // Adds some space between the menu and the buttons
+  },
+  actionCta: {
+    position: "absolute",
+    flexDirection: "column",
+    gap: 20,
+    top: 30,
+    right: 0,
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5,
+    elevation: 3,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
   },
 });
