f50359d985ac4bc65274959017e997a21fe903e2
Babatunde Ademilua <babatundeademilua07@gmail.com>
2025-01-07 14:53:32 +0000

    fix: increase stacking index

--- components/MutateAction.tsx
+++ components/MutateAction.tsx
@@ -42,6 +42,7 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 5,
     elevation: 3,
+    zIndex: 10,
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
